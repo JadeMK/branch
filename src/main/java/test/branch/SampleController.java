2b4d4f3ab735706acf6b0afce948014ddf829933
Jade Kim <JadeKim.dev@gmail.com>
2022-10-21 14:23:01 +0900

    Initial commit

---
+++ src/main/java/test/branch/SampleController.java
@@ -0,0 +1,13 @@
+package test.branch;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "before merging";
+    }
+}
